d7a163a94d7f27feec1a0eafeb055cbc1c6f8a17
terryspitz <terryspitz@gmail.com>
2020-06-07 00:04:18 +0100

    Move svg cropping to pre-project back.

--- src/App.fs
+++ src/App.fs
@@ -1,66 +1,12 @@
 module App
 
-open Browser.Dom
 open System
 open System.Diagnostics
-open Browser.Types
 open Fable.Core
-open Browser
+open Browser.Types
+open Browser.Dom
 
 open Pool
-
-let drawCaustics (ctx : CanvasRenderingContext2D) time = 
-    let debug = false
-    let mutable minmaxarea = (1.,0.) 
-    let res = 10.
-    let w = int (ctx.canvas.width / res)
-    let h = int (ctx.canvas.height / res)
-    let scale = float (min w h)
-    let ds = 1. / float scale
-    let margin = 5
-    let mutable derivs1 = Array.create (w+margin*2) (0.,0.)
-    let mutable derivs2 = Array.create (w+margin*2) (0.,0.)
-    for col in -margin..w+margin do
-        derivs1.[col+margin] <- CalcDerivs (float col * ds) 0. time
-    for row in -margin..h+margin-1 do
-        let py = float row * ds
-        for col in -margin..w+margin do
-            derivs2.[col+margin] <- CalcDerivs (float col * ds) (py+ds) time
-        for col in -margin..w+margin-1 do
-            let px = float col * ds
-            let xtl = px    + fst derivs1.[col+margin]
-            let ytl = py    + snd derivs1.[col+margin]
-            let xtr = px+ds + fst derivs1.[col+margin+1]
-            let ytr = py    + snd derivs1.[col+margin+1]
-            let xbl = px    + fst derivs2.[col+margin]
-            let ybl = py+ds + snd derivs2.[col+margin]
-            let xbr = px+ds + fst derivs2.[col+margin+1]
-            let ybr = py+ds + snd derivs2.[col+margin+1]
-            // printfn "\n%d %d M %f , %f L %f , %f L %f , %f L %f , %f" row col xtl ytl xtr ytr xbr ybr xbl ybl
-
-            let cross x1 y1 x2 y2 = abs (x1*y2-x2*y1)
-            let area = (cross (xtr-xtl) (ytr-ytl) (xbl-xtl) (ybl-ytl) + cross (xtr-xbr) (ytr-ybr) (xbl-xbr) (ybl-ybr) ) / 2. * scale * scale
-            ctx.beginPath()
-            ctx.moveTo(xtl, ytl)
-            let offset = -0.000
-            ctx.lineTo(xtr-offset, ytr)
-            ctx.lineTo(xbr-offset, ybr-offset)
-            ctx.lineTo(xbl, ybl-offset)
-            ctx.closePath()
-            let alpha = min (0.6/area) 1.0
-            let colour = sprintf "rgba(255,255,255,%f)" alpha
-            ctx.fillStyle <- U3.Case1 colour
-            if debug then
-                printfn "%A" colour
-            ctx.fill()
-            if debug then
-                minmaxarea <- (min alpha (fst minmaxarea), max alpha (snd minmaxarea))
-    
-        for col in -margin..w+margin do
-            derivs1.[col+margin] <- derivs2.[col+margin]
-
-    if debug then
-        printfn "%A" minmaxarea
 
 
 let intervalMs = 0.08
@@ -73,15 +19,16 @@ ctx.strokeStyle <- U3.Case1 "#000000"
 let update() =
     let start = DateTime.UtcNow.Ticks
     timeMs <- timeMs + intervalMs
-    // pool.innerHTML <- String.concat "\n" (poolHtml timeMs)
+    pool.innerHTML <- String.concat "\n" (poolHtml timeMs)
     canvas.width <- window.innerWidth
     canvas.height <- window.innerHeight
     let ctx = canvas.getContext_2d()
     ctx.setTransform(1., 0., 0., 1., 0., 0.)
-    ctx.scale(canvas.height, canvas.height)
+    // ctx.scale(canvas.height, canvas.height)
+    ctx.scale(100., 100.)
     ctx.fillStyle <- U3.Case1 "#146897" //from pool.jpg
     ctx.fillRect(0., 0., canvas.width, canvas.height)
-    drawCaustics ctx timeMs
+    // drawCaustics ctx timeMs
 
     printfn "%d ms" ((DateTime.UtcNow.Ticks-start)/10000L)
 
--- src/Pool.fs
+++ src/Pool.fs
@@ -2,8 +2,17 @@ module Pool
 
 open System
 open System.Diagnostics
+open Fable.Core
+open Browser
+open Browser.Types
 
 open MyArray2D
+
+//TODO:
+// apply caustics and projection together
+// add specular highlights
+// reimplement in Three.js shader (!)
+
 
 let tau = 2.0 * Math.PI
 let frequencies = 11
@@ -29,7 +38,7 @@ let fastCos x =
 
 let CalcDerivs x y time =
     let speed = 0.05
-    let scaling = 0.08
+    let scaling = 0.03
     let sgn x = if x<0 then -1. else 1.
     let mutable dx, dy = 0., 0.
     for i in -frequencies..frequencies do
@@ -42,17 +51,75 @@ let CalcDerivs x y time =
             dy <- dy + amp * (float j) * costheta
     dx*scaling, dy*scaling
 
+let drawCaustics (ctx : CanvasRenderingContext2D) time = 
+    let debug = false
+    let mutable minmaxarea = (1.,0.) 
+    let res = 75.
+    let w = int (ctx.canvas.width / res)
+    let h = int (ctx.canvas.height / res)
+    let scale = float (min w h)
+    let ds = 1. / float scale
+    let ds2 = ds/2.0
+    let margin = 5
+    let mutable derivs1 = Array.create (w+margin*2) (0.,0.)
+    let mutable derivs2 = Array.create (w+margin*2) (0.,0.)
+    for col in -margin..w+margin do
+        derivs1.[col+margin] <- CalcDerivs (float col * ds) (float -margin * ds) time
+    for row in -margin..h+margin-1 do
+        let py = float row * ds
+        for col in -margin..w+margin do
+            derivs2.[col+margin] <- CalcDerivs (float col * ds) (py+ds) time
+        for col in -margin..w+margin-1 do
+            let px = float col * ds
+            let xtl = px-ds2 + fst derivs1.[col+margin]
+            let ytl = py-ds2 + snd derivs1.[col+margin]
+            let xtr = px+ds2 + fst derivs1.[col+margin+1]
+            let ytr = py-ds2 + snd derivs1.[col+margin+1]
+            let xbl = px-ds2 + fst derivs2.[col+margin]
+            let ybl = py+ds2 + snd derivs2.[col+margin]
+            let xbr = px+ds2 + fst derivs2.[col+margin+1]
+            let ybr = py+ds2 + snd derivs2.[col+margin+1]
+            // printfn "\n%d %d M %f , %f L %f , %f L %f , %f L %f , %f" row col xtl ytl xtr ytr xbr ybr xbl ybl
+
+            let cross x1 y1 x2 y2 = abs (x1*y2-x2*y1)
+            let area = (cross (xtr-xtl) (ytr-ytl) (xbl-xtl) (ybl-ytl) + cross (xtr-xbr) (ytr-ybr) (xbl-xbr) (ybl-ybr) ) / 2. * scale * scale
+            ctx.beginPath()
+            ctx.moveTo(xtl, ytl)
+            let offset = -0.000
+            ctx.lineTo(xtr-offset, ytr)
+            ctx.lineTo(xbr-offset, ybr-offset)
+            ctx.lineTo(xbl, ybl-offset)
+            ctx.closePath()
+            let alpha = min (0.6/area) 1.0
+            let colour = sprintf "rgba(155,255,255,%f)" alpha
+            // let colour = sprintf "#4e727c%x" (int (alpha*255.))
+            ctx.fillStyle <- U3.Case1 colour
+            if debug then
+                printfn "%A" colour
+            ctx.fill()
+            if debug then
+                minmaxarea <- (min alpha (fst minmaxarea), max alpha (snd minmaxarea))
+    
+        for col in -margin..w+margin do
+            derivs1.[col+margin] <- derivs2.[col+margin]
+
+    if debug then
+        printfn "%A" minmaxarea
+
 [<Struct>]
 type Point = { X: float; Y: float }
 with
     override this.ToString() = sprintf "%.4f,%.4f" this.X this.Y
     static member (+) (p1, p2) = {X=p1.X+p2.X; Y=p1.Y+p2.Y}
+    static member (-) (p1, p2) = {X=p1.X-p2.X; Y=p1.Y-p2.Y}
     ///rotates by 90deg clockwise about (0.5,0.5)
     member this.Rotate90C = {X=this.Y; Y=1.0-this.X}
     member this.PosX = this.X > 0.0
 
 type Poly = { pts: Point[] }
 with
+    static member (+) (poly,pt) = {pts=Array.map (fun p -> p+pt) poly.pts}
+    static member (-) (poly,pt) = {pts=Array.map (fun p -> p-pt) poly.pts}
     member this.Rotate90C = {pts=[|for p in this.pts do p.Rotate90C|]}
     member this.CropX =
         match this.pts |> Array.tryFindIndex (fun p -> p.PosX) with
@@ -80,12 +147,11 @@ with
             Debug.Assert(((Array.map (fun p->p.Y) p.pts |> Array.max) <= 1.0), (sprintf "pts: %A, p: %A" this.pts p))
             Debug.Assert(((Array.map (fun p->p.X) p.pts |> Array.max) <= 1.0), (sprintf "pts: %A, p: %A" this.pts p))
         p
-    static member (+) (poly,pt) = {pts=Array.map (fun p -> p+pt) poly.pts}
+    member this.CropUnitSquareAt pt = ((this-pt).CropUnitSquare) + pt
     member this.ToSvg colour =
         if this.pts.Length>0 then
             [
-                "<path "
-                "d='"
+                "<path d='"
                 sprintf "M %s L " (this.pts.[0].ToString())
                 + String.concat " L " [for i in 1..this.pts.Length do this.pts.[i%this.pts.Length].ToString()]
                 "'"
@@ -104,12 +170,12 @@ with
     static member NearUnitSquare = {pts=[|{X=0.1;Y=0.1}; {X=0.1;Y=0.9}; {X=0.9;Y=0.9}; {X=0.9;Y=0.1}|]}
 
 let poolHtml time = 
-    let mult = 1
+    let mult = 2
     let w = 16 * mult
     let h = 9 * mult
     let scale = float (min w h)
     let ds = 1.0 / scale
-    let grid = scale * 2.0
+    let grid = scale * 1.0
     let mutable derivs1 = Array.create (w+1) (0., 0.)
     let mutable derivs2 = Array.create (w+1) (0., 0.)
     for col in 0..w do
@@ -133,15 +199,11 @@ let poolHtml time =
                 let ybr = rr+ds + snd derivs2.[col+1]
                 if debug then
                     printfn "\n%d %d M %f , %f L %f , %f L %f , %f L %f , %f" row col xtl ytl xtr ytr xbr ybr xbl ybl
-                let simple = true
-                let spiro = true
+                let poly = {pts=[|{X=xtl*scale;Y=ytl*scale}; {X=xtr*scale;Y=ytr*scale};
+                                  {X=xbr*scale;Y=ybr*scale}; {X=xbl*scale;Y=ybl*scale}|]}
+                let simple = false
                 if simple then
-                    let poly = {pts=[|{X=xtl*scale;Y=ytl*scale}; {X=xtr*scale;Y=ytr*scale};
-                                {X=xbr*scale;Y=ybr*scale}; {X=xbl*scale;Y=ybl*scale}|]}
                     yield! poly.ToSvg (if ((col+5)%(w/4))<2 then "4444ff" else "none")
-                // elif spiro then
-                //     yield! poly.ToSvg (if ((col+5)%(w/4))<2 then "4444ff" else "none")
-
                 else
                     /// map from projected to coords back to screen coords
                     /// i.e. from refracted bottom of pool to surface
@@ -173,27 +235,33 @@ let poolHtml time =
                     for i in (min xtmin xbmin)..(max xtmax xbmax)-1 do
                       for j in (min ylmin yrmin)..(max ylmax yrmax)-1 do
                         //floor pattern
-                        if ((i+5)%(w/2))<5 then
+                        if i%5<2 || j%5<2 then
                             if debug then
                                 printfn "a-e u,v= %f %f %f %f %f %f %f %f " a b c d e f u v
                                 printfn "i,j= %d %d" i j
-                            let poly = {pts=[|
-                                (screen_coords (float i/grid)      (float j/grid))
-                                (screen_coords (float (i+1)/grid)  (float j/grid))
-                                (screen_coords (float (i+1)/grid)  (float (j+1)/grid))
-                                (screen_coords (float i/grid)      (float (j+1)/grid))
-                            |]}
-                            let check p = not (Double.IsNaN p.X) && not (Double.IsNaN p.Y)
+                            let tile = poly.CropUnitSquareAt {X=float i;Y=float j}
+                            let projTile = {pts=[|for p in tile.pts do screen_coords (p.X/grid) (p.Y/grid)|]}
                             let s0 = {X=float col; Y=float row}
-                            if Array.fold (fun s p -> s&&check p) true poly.pts then
-                                let poly = poly.CropUnitSquare
-                                if debug then
-                                    printfn "poly= %A" ((poly+s0).ToSvg "000000")
-                                //sprintf "<rect x="50" y="20" width="150" height="150"
-                                //style="fill:blue;stroke:pink;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9" />                ]
-                                yield! (poly+s0).ToSvg "4444ff"
-                            // else
-                            //     yield! (Poly.NearUnitSquare+s0).ToSvg "ff0000"
+                            yield! (projTile+s0).ToSvg "4444ff"
+                            // yield! (projTile+s0).ToSvg (sprintf "%x%x%x" (255/i) (255/j) (255/row))
+                            //old:
+                            // let poly = {pts=[|
+                            //     (screen_coords (float i/grid)      (float j/grid))
+                            //     (screen_coords (float (i+1)/grid)  (float j/grid))
+                            //     (screen_coords (float (i+1)/grid)  (float (j+1)/grid))
+                            //     (screen_coords (float i/grid)      (float (j+1)/grid))
+                            // |]}
+                            // let check p = not (Double.IsNaN p.X) && not (Double.IsNaN p.Y)
+                            // let s0 = {X=float col; Y=float row}
+                            // if Array.fold (fun s p -> s&&check p) true poly.pts then
+                            //     let poly = poly.CropUnitSquare
+                            //     if debug then
+                            //         printfn "poly= %A" ((poly+s0).ToSvg "000000")
+                            //     //sprintf "<rect x="50" y="20" width="150" height="150"
+                            //     //style="fill:blue;stroke:pink;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9" />                ]
+                            //     yield! (poly+s0).ToSvg "4444ff"
+                            // // else
+                            // //     yield! (Poly.NearUnitSquare+s0).ToSvg "ff0000"
             for col in 0..w do
                 derivs1.[col] <- derivs2.[col]
         ]
